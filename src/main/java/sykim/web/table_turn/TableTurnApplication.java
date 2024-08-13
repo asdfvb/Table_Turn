cd4d6ccf5b3f4793743041134366e667f1b295fc
sykim <jessic3200@gmail.com>
2024-08-13 16:10:47 +0900

    Table_Turn First Commit

---
+++ src/main/java/sykim/web/table_turn/TableTurnApplication.java
@@ -0,0 +1,13 @@
+package sykim.web.table_turn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TableTurnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TableTurnApplication.class, args);
+	}
+
+}
---
+++ src/test/java/sykim/web/table_turn/TableTurnApplicationTests.java
@@ -0,0 +1,13 @@
+package sykim.web.table_turn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TableTurnApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
