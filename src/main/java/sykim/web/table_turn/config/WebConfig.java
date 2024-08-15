e3a8980eb148d2a950df2a239c662dd42b4c3660
sykim <jessic3200@gmail.com>
2024-08-15 10:19:00 +0900

    jsp연동

---
+++ src/main/java/sykim/web/table_turn/config/WebConfig.java
@@ -0,0 +1,15 @@
+package sykim.web.table_turn.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Component
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        WebMvcConfigurer.super.addCorsMappings(registry);
+    }
+}
---
+++ src/main/java/sykim/web/table_turn/controller/MainController.java
@@ -0,0 +1,13 @@
+package sykim.web.table_turn.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MainController {
+
+    @GetMapping("/main")
+    public String main(){
+        return "main";
+    }
+}
